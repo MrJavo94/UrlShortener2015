87b1a9312c527fc16aefa32880ed1ad2dc47f2db
Alejandro Dieste Cortes <alej.14@hotmail.com>
2015-12-08 14:46:11 +0100

    Mi parte

---
+++ imperial-red/src/main/java/urlshortener2015/imperialred/objects/StatsURL.java
@@ -0,0 +1,46 @@
+package urlshortener2015.imperialred.objects;
+
+public class StatsURL {
+
+    private String target;
+    private String date;
+    private long clicks;
+
+    public StatsURL(String target, String date, long clicks) {
+
+        this.target = target;
+        this.date = date;
+        this.clicks = clicks;
+    }
+
+    public String getTarget() { return target; }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getUsesCount() {
+        return clicks;
+    }
+
+    public void setClicks(int clicks) {
+        this.clicks = clicks;
+    }
+
+    @Override
+    public String toString() {
+        return "StatsURL{" +
+                "longURI=" + target +
+                ", date=" + date +
+                ", clicks=" + clicks +
+                '}';
+    }
+}
---
+++ imperial-red/src/main/java/urlshortener2015/imperialred/web/StatsController.java
@@ -0,0 +1,58 @@
+package urlshortener2015.imperialred.web;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import urlshortener2015.imperialred.objects.StatsURL;
+import urlshortener2015.imperialred.objects.ShortURL;
+import urlshortener2015.imperialred.repository.ClickRepository;
+import urlshortener2015.imperialred.repository.ShortURLRepository;
+
+@Controller
+public class StatsController {
+
+	@Autowired
+	protected ClickRepository clickRepository;
+
+	@Autowired
+	ShortURLRepository shortURLRepository;
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(StatsController.class);
+
+
+	@RequestMapping(value = "/{id:(?!link|index|stats).*}+", method = RequestMethod.GET, produces = "text/html")
+	public String redirectToStatistics(@PathVariable String id,
+			HttpServletRequest request, Model model) {
+
+		logger.info("Requested redirection to statistics with hash " + id);
+		ShortURL l = shortURLRepository.findByHash(id);
+		model.addAttribute("target", l.getTarget());
+		model.addAttribute("date", l.getCreated());
+		model.addAttribute("clicks", clickRepository.clicksByHash(l.getHash()));
+		return "stats";
+	}
+
+	@RequestMapping(value = "/{id:(?!link|index).*}+", method = RequestMethod.GET, produces = "aplication/json")
+	public ResponseEntity<?> statsJSON(@PathVariable String id,
+			HttpServletRequest request) {
+
+		logger.info("Requested json with hash " + id);
+		ShortURL l = shortURLRepository.findByHash(id);
+		StatsURL stats = new StatsURL(l.getTarget(), l.getCreated().toString(),
+				clickRepository.clicksByHash(l.getHash()));
+		return new ResponseEntity<>(stats, HttpStatus.OK);
+	}
+
+}
