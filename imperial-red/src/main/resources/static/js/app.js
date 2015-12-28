170297e739f0792cd00639a226079641bc8a63f9
Javier Beltran <532581@unizar.es>
2015-12-28 18:51:12 +0100

    Added to front-end multi-field email for private links. JS not working.

--- imperial-red/src/main/resources/static/js/app.js
+++ imperial-red/src/main/resources/static/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(
-    function() {
+function() {
     
     $(function () {
         $('.button-checkbox').each(
@@ -152,7 +152,31 @@ $(document).ready(
                     }
                 });
             });
-    });
+});
+
+$(function()
+		{
+		    $(document).on('click', '.btn-add', function(e)
+		    {
+		        e.preventDefault();
+		        console.log('Entered');
+		        var controlForm = $('.controls form:first'),
+		            currentEntry = $(this).parents('.entry:first'),
+		            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+		        newEntry.find('input').val('');
+		        controlForm.find('.entry:not(:last) .btn-add')
+		            .removeClass('btn-add').addClass('btn-remove')
+		            .removeClass('btn-success').addClass('btn-danger')
+		            .html('<span class="glyphicon glyphicon-minus"></span>');
+		    }).on('click', '.btn-remove', function(e)
+		    {
+				$(this).parents('.entry:first').remove();
+
+				e.preventDefault();
+				return false;
+			});
+});
     
     
 function refrescarSugerencia(id) {
