03f24d6736b1dd54eaafe80c67bfaf44649462a8
jorcox <646122@unizar.es>
2016-01-03 12:46:35 +0100

    Sesiones Persistentes Sociales

--- imperial-red/src/main/java/urlshortener2015/imperialred/config/SocialConfig.java
+++ imperial-red/src/main/java/urlshortener2015/imperialred/config/SocialConfig.java
@@ -3,31 +3,93 @@ package urlshortener2015.imperialred.config;
 import javax.inject.Inject;
 
 import org.apache.tomcat.util.net.jsse.openssl.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.social.FacebookAutoConfiguration;
+import org.springframework.boot.autoconfigure.social.TwitterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.social.UserIdSource;
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
+import org.springframework.social.config.annotation.EnableSocial;
+import org.springframework.social.config.annotation.SocialConfiguration;
+import org.springframework.social.config.annotation.SocialConfigurer;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
+import org.springframework.social.security.AuthenticationNameUserIdSource;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
+
+import urlshortener2015.imperialred.repository.MongoConnectionTransformers;
+import urlshortener2015.imperialred.repository.MongoUsersConnectionRepository;
+import urlshortener2015.imperialred.social.GoogleAutoConfiguration;
+import urlshortener2015.imperialred.web.CustomConnectController;
 
 @Configuration
-public class SocialConfig {
+@EnableSocial
+@EnableAutoConfiguration(exclude={GoogleAutoConfiguration.class, TwitterAutoConfiguration.class, FacebookAutoConfiguration.class})
 
-    @Inject
-    private Environment environment;
+public class SocialConfig implements SocialConfigurer{
 	
-    @Bean
-    public ConnectionFactoryLocator connectionFactoryLocator() {
-    	
-        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
-        registry.addConnectionFactory(new GoogleConnectionFactory(
-            environment.getProperty("google.consumerKey"),
-            environment.getProperty("google.consumerSecret")));
-        return registry;
-    }
+	@Autowired
+	MongoTemplate mongoTemplate;
+
+//    @Inject
+//    private Environment environment;
+//	
+//    @Bean
+//    public ConnectionFactoryLocator connectionFactoryLocator() {
+//    	
+//        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+//        registry.addConnectionFactory(new GoogleConnectionFactory(
+//            environment.getProperty("google.consumerKey"),
+//            environment.getProperty("google.consumerSecret")));
+//        return registry;
+//    }
+	
+	
+//    @Bean
+//    public ConnectController connectController(
+//                ConnectionFactoryLocator connectionFactoryLocator,
+//                ConnectionRepository connectionRepository) {
+//        return new ConnectController(connectionFactoryLocator, connectionRepository);
+//    }
+    
+
+	@Override
+	public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer,
+			Environment environment) {
+//		connectionFactoryConfigurer.addConnectionFactory(new TwitterConnectionFactory(
+//				environment.getProperty("spring.social.twitter.appId"),
+//				environment.getProperty("spring.social.twitter.appSecret")));
+//		connectionFactoryConfigurer.addConnectionFactory(new FacebookConnectionFactory(
+//				environment.getProperty("spring.social.facebook.appId"),
+//				environment.getProperty("spring.social.facebook.appSecret")));
+//		connectionFactoryConfigurer.addConnectionFactory(new GoogleConnectionFactory(
+//				environment.getProperty("spring.social.google.appId"),
+//				environment.getProperty("spring.social.google.appSecret")));
+		
+	}
+
+	@Override
+	public UserIdSource getUserIdSource() {
+		return new AuthenticationNameUserIdSource();
+	}
+
+	@Override
+	public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+		return new MongoUsersConnectionRepository(mongoTemplate, connectionFactoryLocator, new MongoConnectionTransformers(connectionFactoryLocator, Encryptors.noOpText()));
+	}
 
 }
 
--- imperial-red/src/main/java/urlshortener2015/imperialred/web/CustomConnectController.java
+++ imperial-red/src/main/java/urlshortener2015/imperialred/web/CustomConnectController.java
@@ -9,9 +9,22 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.social.config.annotation.SocialConfiguration;
+import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionFactory;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.DuplicateConnectionException;
+import org.springframework.social.connect.support.OAuth1ConnectionFactory;
+import org.springframework.social.connect.support.OAuth2ConnectionFactory;
 import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.connect.web.ConnectInterceptor;
+import org.springframework.social.connect.web.ConnectSupport;
+import org.springframework.social.connect.web.HttpSessionSessionStrategy;
+import org.springframework.social.connect.web.SessionStrategy;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.google.api.Google;
 import org.springframework.social.twitter.api.Twitter;
@@ -22,6 +35,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.view.RedirectView;
+
+import urlshortener2015.imperialred.config.SocialConfig;
 
 @Controller
 @Scope("session")
@@ -42,14 +59,101 @@ public class CustomConnectController extends ConnectController {
 	private String mail;
 
 	private ConnectionRepository connectionRepository;
+	
+	private ConnectionFactoryLocator connectionFactoryLocator;
+	
+	private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
+	
+	private ConnectSupport connectSupport;
 
 	@Inject
 	public CustomConnectController(ConnectionFactoryLocator connectionFactoryLocator,
 			ConnectionRepository connectionRepository) {
 		super(connectionFactoryLocator, connectionRepository);
 		this.connectionRepository = connectionRepository;
+		this.connectionFactoryLocator = connectionFactoryLocator;
 		//super.setApplicationUrl("http://ired.ml");
 	}
+	
+	/**
+	 * Process the authorization callback from an OAuth 2 service provider.
+	 * Called after the user authorizes the connection, generally done by having he or she click "Allow" in their web browser at the provider's site.
+	 * On authorization verification, connects the user's local account to the account they hold at the service provider.
+     * @param providerId the provider ID to connect to
+     * @param request the request
+     * @return a RedirectView to the connection status page
+	 */
+	@Override
+	@RequestMapping(value="/{providerId}", method=RequestMethod.GET, params="code")
+	public RedirectView oauth2Callback(@PathVariable String providerId, NativeWebRequest request) {
+		connectSupport = new ConnectSupport(sessionStrategy);
+		try {
+			OAuth2ConnectionFactory<?> connectionFactory = (OAuth2ConnectionFactory<?>) connectionFactoryLocator.getConnectionFactory(providerId);
+			Connection<?> connection = connectSupport.completeConnection(connectionFactory, request);
+			String uniqueId = "";
+			switch(providerId){
+			case("google"):
+				google = (Google) connection.getApi();
+				uniqueId = google.plusOperations().getGoogleProfile().getAccountEmail();
+				break;
+			case("facebook"):
+				facebook = (Facebook) connection.getApi();
+				uniqueId = facebook.userOperations().getUserProfile().getEmail();
+				break;
+			case("twitter"):
+				twitter = (Twitter) connection.getApi();
+				uniqueId = twitter.userOperations().getUserProfile().getScreenName();
+				break;
+			}
+			SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(uniqueId, null, null));			
+			addConnection(connection, connectionFactory, request);
+		} catch (Exception e) {
+			sessionStrategy.setAttribute(request, PROVIDER_ERROR_ATTRIBUTE, e);
+			logger.warn("Exception while handling OAuth2 callback (" + e.getMessage() + "). Redirecting to " + providerId +" connection status page.");
+		}
+		return connectionStatusRedirect(providerId, request);
+	}
+	
+	/**
+	 * Process the authorization callback from an OAuth 1 service provider.
+	 * Called after the user authorizes the connection, generally done by having he or she click "Allow" in their web browser at the provider's site.
+	 * On authorization verification, connects the user's local account to the account they hold at the service provider
+	 * Removes the request token from the session since it is no longer valid after the connection is established.
+     * @param providerId the provider ID to connect to
+     * @param request the request
+     * @return a RedirectView to the connection status page
+	 */
+	@Override
+	@RequestMapping(value="/{providerId}", method=RequestMethod.GET, params="oauth_token")
+	public RedirectView oauth1Callback(@PathVariable String providerId, NativeWebRequest request) {
+		connectSupport = new ConnectSupport(sessionStrategy);
+		try {
+			OAuth1ConnectionFactory<?> connectionFactory = (OAuth1ConnectionFactory<?>) connectionFactoryLocator.getConnectionFactory(providerId);
+			Connection<?> connection = connectSupport.completeConnection(connectionFactory, request);
+			String uniqueId = "";
+			switch(providerId){
+			case("google"):
+				google = (Google) connection.getApi();
+				uniqueId = google.plusOperations().getGoogleProfile().getAccountEmail();
+				break;
+			case("facebook"):
+				facebook = (Facebook) connection.getApi();
+				uniqueId = facebook.userOperations().getUserProfile().getEmail();
+				break;
+			case("twitter"):
+				twitter = (Twitter) connection.getApi();
+				uniqueId = twitter.userOperations().getUserProfile().getScreenName();
+				break;
+			}
+			SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(uniqueId, null, null));
+			addConnection(connection, connectionFactory, request);
+		} catch (Exception e) {
+			sessionStrategy.setAttribute(request, PROVIDER_ERROR_ATTRIBUTE, e);
+			logger.warn("Exception while handling OAuth1 callback (" + e.getMessage() + "). Redirecting to " + providerId +" connection status page.");
+		}
+		return connectionStatusRedirect(providerId, request);
+	}
+	
 
 	protected String connectedView(String providerId) {
 		if (providerId.equals("facebook")) {
@@ -70,12 +174,12 @@ public class CustomConnectController extends ConnectController {
 			logger.info("Mail: " + google.plusOperations().getGoogleProfile().getAccountEmail());
 		}
 		
-		connectionRepository.removeConnections(providerId);
+		//connectionRepository.removeConnections(providerId);
 		return "redirect:/";
 	}
 
 	@RequestMapping(value = "/{providerId}/mail", method = RequestMethod.POST)
-	public ResponseEntity<?> connect(@PathVariable String providerId, NativeWebRequest request,
+	public ResponseEntity<?> mailChecker(@PathVariable String providerId, NativeWebRequest request,
 			@RequestParam(value = "mail", required = true) String mail) {
 		this.mail = mail;
 		HttpHeaders h = new HttpHeaders();
@@ -95,5 +199,21 @@ public class CustomConnectController extends ConnectController {
 			return new ResponseEntity<>(h, HttpStatus.FORBIDDEN);
 		}
 	}
+	
+	private void addConnection(Connection<?> connection, ConnectionFactory<?> connectionFactory, WebRequest request) {
+		try {
+			connectionRepository.addConnection(connection);
+			//postConnect(connectionFactory, connection, request);
+		} catch (DuplicateConnectionException e) {
+			sessionStrategy.setAttribute(request, DUPLICATE_CONNECTION_ATTRIBUTE, e);
+		}
+	}
+	
+//	@SuppressWarnings({ "rawtypes", "unchecked" })
+//	private void postConnect(ConnectionFactory<?> connectionFactory, Connection<?> connection, WebRequest request) {
+//		for (ConnectInterceptor interceptor : interceptingConnectionsTo(connectionFactory)) {
+//			interceptor.postConnect(connection, request);
+//		}
+//	}
 
 }
