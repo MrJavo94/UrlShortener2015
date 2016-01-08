53c24811aa1b83d2b356ab438d741d61ffcaaab7
jorcox <646122@unizar.es>
2016-01-08 15:45:37 +0100

    Algun cambio

--- imperial-red/src/main/java/urlshortener2015/imperialred/config/WebSecurityConfig.java
+++ imperial-red/src/main/java/urlshortener2015/imperialred/config/WebSecurityConfig.java
@@ -21,7 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                	.antMatchers("/**,/**/**", "/home").permitAll()
+                	.antMatchers("/**,/**/**", "/home")
+                	.permitAll()
             .and()
             	.formLogin()
                 .loginPage("/login")
